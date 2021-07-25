a0b9b4ee2ae60d2433223bebd589d572d56d014f
theaellengo <theaellengo@gmail.com>
2021-07-25 17:23:35 +0800

    added ghost prefab and script

---
+++ Assets/Scripts/GhostSpawn.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject ghost;
+    [SerializeField] private List<GameObject> ghosts;
+
+    public const string NUM_SPAWNS_KEY = "NUM_SPAWNS_KEY";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.ghost.SetActive(false);
+        EventBroadcaster.Instance.AddObserver(EventNames.S12_Events.ON_SPAWN_BUTTON_CLICKED, this.OnSpawnEvent);
+    }
+
+    private void OnDestroy()
+    {
+        EventBroadcaster.Instance.RemoveObserver(EventNames.S12_Events.ON_SPAWN_BUTTON_CLICKED);
+    }
+
+    private void OnSpawnEvent(Parameters parameters)
+    {
+        int nSpawns = parameters.GetIntExtra(NUM_SPAWNS_KEY, 1);
+
+        for (int i = 0; i < nSpawns; i++)
+        {
+            GameObject copy = GameObject.Instantiate(ghost, this.transform);
+            copy.SetActive(true);
+            this.ghosts.Add(copy);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
